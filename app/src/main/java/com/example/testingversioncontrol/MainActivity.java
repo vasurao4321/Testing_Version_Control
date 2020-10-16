7635cfa241769d250785e57296b3d612595ef06c
vasurao4321 <64043935+vasurao4321@users.noreply.github.com>
2020-10-15 17:40:54 -0700

    Initial commit

---
+++ app/src/main/java/com/example/testingversioncontrol/MainActivity.java
@@ -0,0 +1,23 @@
+
+public class MainActivity {
+
+    public static void main(String[] args) {
+        int[] Names;
+        Names = new int[3];
+
+        Names[0] = 2;
+
+        Names[1] = 23;
+
+        Names[2] = 22;
+
+        System.out.println("array [0] = " + Names[0]);
+
+        System.out.println("array [1] = " + Names[1]);
+
+        System.out.println("array [2] = " + Names[2]);
+    }
+}
+
+
+
